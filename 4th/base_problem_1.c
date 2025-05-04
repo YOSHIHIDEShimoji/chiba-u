b9f2c5fe077fdc77ede986d15bbf98c95b259b60
YOSHIHIDEShimoji <g.y.shimoji@gmail.com>
2025-05-04 23:05:47 +0900

    make vic.sh

---
+++ 4th/base_problem_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0, b = 3, c =2;
+	a = b = c;
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+	printf("c = %d\n", c);
+
+	return 0;
+}
--- Makefile
+++
@@ -1,6 +0,0 @@
-# 任意の .c ファイルを make コマンドだけでコンパイルできる
-# 例: make hello → cc hello.c -o hello
-
-%: %.c
-	cc $< -o $@
-
