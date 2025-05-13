4272e9c374ecdfab41e84810962944cb3199a9d4
Yoshihide Shimoji <g.y.shimoji@gmail.com>
2025-05-13 20:49:35 +0900

    option.c を更新

--- 5th/option.c
+++ 5th/option.c
@@ -2,16 +2,16 @@
 
 int main(void)
 {
-	int i;
+int i;
 	for (i = 2000; i <= 2200; i++) {
-		if (i % 4 == 0) {
-			if (i % 100 == 0) {
-				if (i % 400 == 0) {
-					printf("%d\n", i);
-				}
-			} else {
-				printf("%d\n", i);
-			}
+		if (i % 400 == 0) {
+			printf("%d\n", i);
 		}
-    }
+		else if (i % 100 == 0) {
+		}
+		else if (i % 4 == 0) {
+			printf("%d\n", i);
+		}
+	}
+	return 0;
 }
