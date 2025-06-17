54711272c2a7a62e54e955aa6f1ae9462ab958f6
YOSHIHIDEShimoji <g.y.shimoji@gmail.com>
2025-06-17 11:58:17 +0900

    10thの課題終了。

--- c/10th/spe_problem_3.c
+++ c/10th/spe_problem_3.c
@@ -3,8 +3,20 @@
 int main(void)
 {
 	char s[70], *ptr1 = s;
+	int i = 0;
 
 	printf("Enter the strings: ");
 	scanf("%s", s);
+	
+	while (s[i]) {
+		i++;
+		ptr1++;
+	}
 
+	while (--i >= 0) {
+		putchar(*(--ptr1));
+	}
+	putchar('\n');
+
+	return 0;
 }
