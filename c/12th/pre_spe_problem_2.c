59c863884e9ac8b06e4f204db8fe8a46ce98ef66
YOSHIHIDEShimoji <g.y.shimoji@gmail.com>
2025-06-22 17:16:02 +0900

    12_2プリプロセッサの課題終了。

---
+++ c/12th/pre_spe_problem_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define add3(a, b, c) ((a) + (b) + (c))
+
+int main(void)
+{
+	printf("%d\n", (add3(1, 2, 3)) * (add3(2 + 2 , 5, 6)));
+	return 0;
+}
---
+++ c/12th/pre_spe_problem_3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define putsd(x) printf("%d\n", (x))
+
+int main(void)
+{
+	int x = 123;
+	putsd(x);
+	return 0;
+}
