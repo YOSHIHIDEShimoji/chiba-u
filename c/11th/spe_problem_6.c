64010fc9e26ebed7ac73aa9f113bbb5d7e5ccc1d
YOSHIHIDEShimoji <g.y.shimoji@gmail.com>
2025-06-24 11:56:05 +0900

    spe_5.cができていない。

---
+++ c/11th/spe_problem_6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int compare(char str1[], char str2[]);
+
+int main(void)
+{
+	int result;
+	char s1[100], s2[100];
+	printf("１つ目の文字列は？：");
+	scanf("%s", s1);
+	printf("２つ目の文字列は？：");
+	scanf("%s", s2);
+	result = compare(s1, s2);
+	printf("%d\n", result);
+	return 0;
+}
+
+int compare(char str1[], char str2[])
+{
+	int i = 0;
+	while (str1[i] != 0) {
+		if (str1[i] == str2[i]) {
+			break;
+		} else {
+			return 0;
+		}
+		i++;
+	}
+	return 1;
+}
