14234475347b033c3b1991372382626b6626eff0
YOSHIHIDEShimoji <g.y.shimoji@gmail.com>
2025-06-24 12:15:36 +0900

    11th/spe_4, 5.c完了。11thの課題終了。

--- c/11th/spe_problem_4.c
+++ c/11th/spe_problem_4.c
@@ -4,14 +4,14 @@ int count_c(char str[], char c);
 
 int main(void)
 {
-	char s[100], c[1];
+	char s[100], c;
 	int number;
 	printf("文字列は？：");
 	scanf("%s%*c", s);
 	printf("探す文字は？：");
-	scanf("%s%*c", c);
-	number = count_c(s, c[1]);
-	printf("文字%cは%sの中に%dこあります\n", c[1], s, number);
+	scanf("%c", &c);
+	number = count_c(s, c);
+	printf("文字%cは%sの中に%dこあります\n", c, s, number);
 	return 0;
 }
 
