4a364f1c914309c9e850c2aec9348253152b17a8
YOSHIHIDEShimoji <g.y.shimoji@gmail.com>
2025-07-01 12:30:23 +0900

    12th/struct終了

---
+++ c/12th/struct_spe_problem_1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct vector
+{
+	int x, y;
+};
+
+struct vector v_2bai(struct vector v);
+
+int main(void)
+{
+	struct vector a = {1, 2}, b;
+	printf("a = (%d, %d)\n", a.x, a.y);
+	b = v_2bai(a);
+	printf("b = (%d, %d)\n", b.x, b.y);
+}
+
+struct vector v_2bai(struct vector v)
+{
+	v.x *= 2;
+	v.y *= 2;
+	return v;	
+}
---
+++ c/12th/struct_spe_problem_3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct person
+{
+	char name[10];
+	struct person *partner;
+	struct person *child;
+	struct person *mother;
+} p0 = {"Taro", (struct person *) 0, (struct person *) 0, (struct person *) 0},
+  p1 = {"Hanako", (struct person *) 0, &p0, (struct person *) 0},
+  p2 = {"Hiroshi", &p1, &p0, (struct person *) 0};
+
+int main(void)
+{
+	p1.partner = &p2;
+	p0.mother = &p1;
+	printf("Mother's name = %s\n", p0.mother->name);
+	printf("Father's name = %s\n", p0.mother->partner->name);
+}
