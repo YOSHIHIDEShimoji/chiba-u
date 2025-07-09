2bd25e8ea3bb0a3ce80d05b2327cfded81e8ffca
YOSHIHIDEShimoji <g.y.shimoji@gmail.com>
2025-07-09 22:01:00 +0900

    speが難しい。if文じゃなくてwhile文使ったほうが良さそう

--- c/14th/spe_problem.c
+++ c/14th/spe_problem.c
@@ -5,24 +5,44 @@ int main(void)
 {
 	FILE *infile = fopen("inputdata.txt", "r"), 
 		 *outfile = fopen("outputdata.csb", "w");	
-	char s[40], ts[15];
+	char s[40], ts[15], as[15];
+	double time[51], signal_A[51], signal_B[51];
+
 
 	if (infile == NULL) {
 		printf("can't open file inputdata.txt\n");
 		exit (1);
 	}
 
+	// time[51]の設定
 	fgets(s, 39, infile);
-	for (int i = 0; i < 51; i++) {
+	int t;
+	for (t = 0; t < 51; t++) {
 		fgets(s, 39, infile);
-		printf("%s", s);
+		int i, j;
+		for (i = 0, j = 0; s[i] != '\t'; i++, j++) {
+			ts[i] = s[i];
+		}
+		ts[i] = '\0';
+		time[t] = atof(ts);
+		// printf("%f\n", time[t]);
 	}
+
 	
-	int i, j;
-	for (i = 0, j = 0; s[i] != '\t'; j++, i++) {
-		ts[i] = s[i];
+	// signal_A[51]の設定
+	fseek(infile, 0, 0);
+	fgets(s, 39, infile);
+	int a;
+	for (a = 0; a < 51; a++) {
+		fgets(s, 39, infile);
+		int i, j;
+		for (i = 0, j = 0; s[i] != '\t'; i++, j++) {
+			as[i] = s[i];
+		}
+		as[i] = '\0';
+		signal_A[a] = atof(as);
+		printf("%f\n", signal_A[a]);
 	}
-	ts[i] = '\0';
-	printf("%s\n", ts);
 
 }
+
