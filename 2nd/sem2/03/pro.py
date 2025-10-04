081c7e640797c4901a09c0553fe91bda549bc1c0
Yoshihide Shimoji <g.y.shimoji@gmail.com>
2025-10-04 09:11:33 +0900

    03/pro.py完了

--- 2nd/sem2/03/pro.py
+++ 2nd/sem2/03/pro.py
@@ -1,23 +1,20 @@
 import random
 
-def bubble_sort(num):
-    print(num)
-    for i in range(9):
-        for j in range(i, 9):
-            if num[i] > num[i + 1]:
-                num[i], num[i + 1] = num[i + 1], num[i]
-
-    print(num)
-            
-
+def bubble_sort(l):
+    for i in range(len(l) - 1):
+        for j in range(len(l) - 1):
+           if l[j] > l[j + 1]:
+               l[j], l[j + 1] = l[j + 1], l[j]
+               j += 1
 
 def main():
-    num = []
+    l = []
     for _ in range(10):
-        v = random.randrange(0, 100)
-        num.append(v)
-
-    bubble_sort(num)
+        v = random.randrange(-100, 100)
+        l.append(v)
+    print('before:', l)
+    bubble_sort(l)
+    print('after: ', l)
     
 if __name__ == '__main__':
     main()
