4870005ce4765c1be3caf1cc581940ef7ac4eb5a
Yoshihide Shimoji <g.y.shimoji@gmail.com>
2025-10-08 18:11:19 +0900

    c1_1終了

--- medexp1/c1/c1_kadai1.c
+++ medexp1/c1/c1_kadai1.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 2000
 #define BUF_SIZE 256
 
 int main()
@@ -13,16 +12,28 @@ int main()
         return 1;
     }
 
-    /* read data */
+    /* define N */
+    int N;
     char buf[BUF_SIZE];
+    fgets(buf, BUF_SIZE - 1, fp);
+    sscanf(buf, "%d\n", &N);
+
+    /* read data */
     int etime = 0, resp = 0;
     int i;
     for (i = 0; i < N; i ++) {
-        fgets(buf, BUF_SIZE-1, fp);
+        fgets(buf, BUF_SIZE - 1, fp);
         sscanf(buf, "%d,%d\n", &etime, &resp);
-        printf("%dms:%d\n", etime, resp);
-    }
 
+        /* print */
+        if (0 <= i && i < 3) {
+            printf("%dms:%d\n", etime, resp);
+        }
+        if (N - 3 <= i && i < N) {
+            printf("%dms:%d\n", etime, resp);
+        }
+    }
+    
     /* ending */
     fclose(fp);
 }
