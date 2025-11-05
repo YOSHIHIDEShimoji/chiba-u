643f394d0cf9a57c2987c360494e0f60186de79a
Yoshihide Shimoji <g.y.shimoji@gmail.com>
2025-11-05 19:05:34 +0900

    05の課題終了

---
+++ prog_spe2/05/pro.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('ecg4000.csv', skiprows=1, names=['Time', 'Voltage'])
+
+plt.figure(figsize=(8, 3))
+plt.plot(df['Time'], df['Voltage'], color='blue', lw=0.8)
+plt.scatter(df['Time'][::50], df['Voltage'][::50], color='red', s=10, label='Sample Points (every 50)')
+plt.title('ECG Signal')
+plt.xlabel('Time [s]')
+plt.ylabel('Voltage [mv]')
+plt.grid(True)
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.savefig('./ecg_waveform.png', dpi=300)
+
+plt.figure(figsize=(6, 3))
+plt.plot(df['Time'], df['Voltage'], color='blue', lw=0.8)
+plt.scatter(df['Time'][::50], df['Voltage'][::50], color='red', s=10, label='Sample Points (every 50)')
+plt.title('Zoomed ECG Signal')
+plt.xlabel('Time [s]')
+plt.ylabel('Voltage [mv]')
+plt.xlim(1, 2)
+plt.grid(True)
+plt.legend(loc='upper right')
+plt.tight_layout()
+plt.savefig('./ecg_zoom.png', dpi=300)
