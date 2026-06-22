f1878b31a29d943fb5c05600567c28454ad2517a
Yoshihide Shimoji <g.y.shimoji@gmail.com>
2026-06-22 09:13:02 +0900

    add setup_python_wsl.sh

---
+++ med-data-analysis/python/setup_python_wsl.sh
@@ -0,0 +1,12 @@
+# powershellで
+# wsl
+
+# wslで
+sudo apt update && \
+sudo apt install -y python3 python3-pip python3-venv && \
+mkdir -p ~/med-data-analysis && \
+cd ~/med-data-analysis && \
+python3 -m venv venv && \
+source venv/bin/activate && \
+python -m pip install --upgrade pip && \
+echo "SUCCESS"
