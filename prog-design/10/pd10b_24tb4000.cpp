f254d8d4fcc20cf48e86e46093ed597bf9cd3964
Yoshihide Shimoji <g.y.shimoji@gmail.com>
2026-07-05 23:42:05 +0900

    add original code pd10b

---
+++ prog-design/10/pd10b_24tb4000.cpp
@@ -0,0 +1,10 @@
+// 課題： pd10b 行列クラスの汎用化 課題
+// 氏名：　　　学生証番号：　　　日付：2026/  /
+// 開発環境：Wandbox(online) https://wandbox.org/
+// コンパイラー：gcc 13.2.0
+// コマンド：g++ prog.cc -Wall -Wextra -std=c++17 -pedantic
+// 説明：
+
+#include <iostream>
+#include <stdlib.h>
+
